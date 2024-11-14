f17ac7bc38304a8942cb20a25000c27e2004843d
Milan Mažgút <mazgut14@stud.uniza.sk>
2024-11-14 21:40:37 +0100

    Home page pomocou product carty

---
+++ SemestralnaPraca/semestralnapraca.client/src/components/ProductCard.tsx
@@ -0,0 +1,27 @@
+import { Link } from 'react-router-dom';
+
+type ProductCardProps = {
+    image: string;
+    title: string;
+    description: string;
+    link: string;
+};
+
+const ProductCard: React.FC<ProductCardProps> = ({ image, title, description, link }) => {
+    return (
+        <div className="col-lg-4 mb-4">
+            <div className="card">
+                <img src={image} className="card-img-top" alt={title} />
+                <div className="card-body">
+                    <h5 className="card-title">{title}</h5>
+                    <p className="card-text">{description}</p>
+                    <Link to={link} className="btn btn-primary">
+                        Viac informácií
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ProductCard;
--- SemestralnaPraca/semestralnapraca.client/src/pages/HomePage.tsx
+++ SemestralnaPraca/semestralnapraca.client/src/pages/HomePage.tsx
@@ -1,70 +1,68 @@
-
 import { Link } from 'react-router-dom';
+import ProductCard from '../components/ProductCard';
 
-const HomePage = () => {
-  return (
-    <div>
-      <div className="container-lg mt-5">
-        <h1 className="display-4">Vitajte na našej stránke!</h1>
-        <p className="lead">
-          Nájdete tu profesionálne klampiarske služby a kvalitné produkty.
-        </p>
-      </div>
+type Product = {
+    image: string;
+    title: string;
+    description: string;
+    link: string;
+};
 
-      <div className="container-lg mt-5">
-        <h2>Naša ponuka</h2>
-        <div className="row">
-          {/* Karta 1 */}
-          <div className="col-lg-4 mb-4">
-            <div className="card">
-              <img src="/images/strecha.jpg" className="card-img-top" alt="Strecha" />
-              <div className="card-body">
-                <h5 className="card-title">Strechy</h5>
-                <p className="card-text">Popis produktu 1.</p>
-                <Link to="/strechy" className="btn btn-primary">
-                  Viac informácií
-                </Link>
-              </div>
+const products: Product[] = [
+    {
+        image: '/images/strecha.jpg',
+        title: 'Strechy',
+        description: 'Popis produktu 1.',
+        link: '/strechy',
+    },
+    {
+        image: '/images/komin.jpg',
+        title: 'Komíny',
+        description: 'Popis produktu 2.',
+        link: '/komin',
+    },
+    {
+        image: '/images/ostatne.jpg',
+        title: 'Iné výrobky',
+        description: 'Popis produktu 3.',
+        link: '/produkty',
+    },
+];
+
+const HomePage: React.FC = () => {
+    return (
+        <div>
+            <div className="container-lg mt-5">
+                <h1 className="display-4">Vitajte na našej stránke!</h1>
+                <p className="lead">
+                    Nájdete tu profesionálne klampiarske služby a kvalitné produkty.
+                </p>
             </div>
-          </div>
-          {/* Karta 2 */}
-          <div className="col-lg-4 mb-4">
-            <div className="card">
-              <img src="/images/komin.jpg" className="card-img-top" alt="Komín" />
-              <div className="card-body">
-                <h5 className="card-title">Komíny</h5>
-                <p className="card-text">Popis produktu 2.</p>
-                <Link to="/komin" className="btn btn-primary">
-                  Viac informácií
-                </Link>
-              </div>
+
+            <div className="container-lg mt-5">
+                <h2>Naša ponuka</h2>
+                <div className="row">
+                    {products.map((product, index) => (
+                        <ProductCard
+                            key={index}
+                            image={product.image}
+                            title={product.title}
+                            description={product.description}
+                            link={product.link}
+                        />
+                    ))}
+                </div>
             </div>
-          </div>
-          {/* Karta 3 */}
-          <div className="col-lg-4 mb-4">
-            <div className="card">
-              <img src="/images/ostatne.jpg" className="card-img-top" alt="Iné" />
-              <div className="card-body">
-                <h5 className="card-title">Iné výrobky</h5>
-                <p className="card-text">Popis produktu 3.</p>
-                <Link to="/produkty" className="btn btn-primary">
-                  Viac informácií
+
+            <div className="container-lg mt-5">
+                <hr className="my-4" />
+                <p>Pre prístup k správe firmy a skladu sa prosím prihláste.</p>
+                <Link className="btn btn-primary btn-lg" to="/login" role="button">
+                    Prihlásiť sa
                 </Link>
-              </div>
             </div>
-          </div>
         </div>
-      </div>
-
-      <div className="container-lg mt-5">
-        <hr className="my-4" />
-        <p>Pre prístup k správe firmy a skladu sa prosím prihláste.</p>
-        <Link className="btn btn-primary btn-lg" to="/login" role="button">
-          Prihlásiť sa
-        </Link>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default HomePage;
